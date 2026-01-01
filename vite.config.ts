262622b619adf69fce30999f62d088a46d4c9eca
wcorsini79 <136508421+wcorsini79@users.noreply.github.com>
2026-01-01 11:54:22 -0300

    Updated package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
-import { jsxLocPlugin } from "@builder.io/vite-plugin-jsx-loc";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import fs from "node:fs";
 import path from "path";
 import { defineConfig } from "vite";
 import { vitePluginManusRuntime } from "vite-plugin-manus-runtime";
 
 
-const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime()];
+const plugins = [react(), tailwindcss(), vitePluginManusRuntime()];
 
 export default defineConfig({
   plugins,
